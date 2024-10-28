4a366c7b43cb88f0be14f9698eeb9e67cf541937
suryathink <suryaprakashsingh110@gmail.com>
2024-10-29 00:16:29 +0530

    fixes csv code

--- server/src/utils/csvProcessor.ts
+++ server/src/utils/csvProcessor.ts
@@ -1,14 +1,13 @@
-// src/utils/csvProcessor.ts
 import csv from "csv-parser";
 import fs from "fs";
-import { insertDataInBatches } from "./database";
 import log4js from "log4js";
+import { insertDataInBatches } from "./database";
 
 const logger = log4js.getLogger();
 
 export const processCSV = async (filePath: string) => {
-  const rows: any[] = []; // Temporary storage for each batch of data
-  const batchSize = 50; // Number of records to process in each batch
+  const rows: any[] = [];
+  const batchSize = 50;
 
   // Stream the CSV file
   fs.createReadStream(filePath)
@@ -18,20 +17,17 @@ export const processCSV = async (filePath: string) => {
         rows.push(data);
       }
 
-      // If the rows array reaches the batch size, insert it into the database
       if (rows.length === batchSize) {
         insertDataInBatches(rows.slice());
-        rows.length = 0; // Clear the batch after insertion
+        rows.length = 0;
       }
     })
     .on("end", async () => {
-      // Insert any remaining records if they didn't complete a full batch
       if (rows.length) {
         await insertDataInBatches(rows);
       }
       logger.log("CSV file successfully processed");
 
-      // Delete the CSV file after processing
       fs.unlink(filePath, (err) => {
         if (err) {
           logger.error(`Error deleting file: ${filePath}`, err);
